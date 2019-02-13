83bb3dce9eea3bff826bbc46734d8b7e87ffbafa
Deepika Mandloi <deepikam@bitwiseglobal.net>
2019-02-13 21:16:06 +0530

    sample project

---
+++ src/main/java/com/bitwise/jll/demo/jllSpringboot/JllSpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.bitwise.jll.demo.jllSpringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JllSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JllSpringBootApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/bitwise/jll/demo/jllSpringboot/JllSpringBootApplicationTests.java
@@ -0,0 +1,17 @@
+package com.bitwise.jll.demo.jllSpringboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JllSpringBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
